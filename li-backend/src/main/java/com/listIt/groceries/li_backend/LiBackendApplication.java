5e7882a3171eea2590cc2e5da64e30aedb2863ac
Gianluca Girardi <gl.girardi1@gmail.com>
2025-05-25 18:09:22 -0400

    Initial commit: added Expo frontend and Spring Boot backend setup

---
+++ li-backend/src/main/java/com/listIt/groceries/li_backend/LiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.listIt.groceries.li_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiBackendApplication.class, args);
+	}
+
+}
---
+++ li-backend/src/test/java/com/listIt/groceries/li_backend/LiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listIt.groceries.li_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
