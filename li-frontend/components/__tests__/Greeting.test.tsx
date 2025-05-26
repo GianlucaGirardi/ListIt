9b409b58ddf39fd6ac72afc9442f6b3ed6f79dfa
Gianluca Girardi <gl.girardi1@gmail.com>
2025-05-25 20:57:00 -0400

    Testing ci-cd unit test step

--- li-frontend/components/__tests__/Greeting.test.tsx
+++ li-frontend/components/__tests__/Greeting.test.tsx
@@ -6,6 +6,6 @@ describe("Greeting Component", () => {
   it("renders the correct name", () => {
     const { getByTestId } = render(<Greeting name="Luca" />);
     const text = getByTestId("greeting-text");
-    expect(text.props.children).toContain("Luca");
+    expect(text.props.children).toContain("Luc");
   });
 });
